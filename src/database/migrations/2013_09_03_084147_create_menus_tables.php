05602b6df025f23960cdbdc3ca1d9232a56e7b17
Samuel De Backer <sdebacker@gmail.com>
2015-03-01 21:15:04 +0100

    boolean in migration

--- src/database/migrations/2013_09_03_084147_create_menus_tables.php
+++ src/database/migrations/2013_09_03_084147_create_menus_tables.php
@@ -29,7 +29,7 @@ class CreateMenusTables extends Migration
             $table->integer('menu_id')->unsigned();
 
             $table->string('locale')->index();
-            $table->tinyInteger('status')->default(0);
+            $table->boolean('status')->default(0);
 
             $table->string('title');
 
