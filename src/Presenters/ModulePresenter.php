9db99d481fd87ad12a7d65f51ebdb2d75a32e208
Samuel De Backer <sdebacker@gmail.com>
2015-05-10 14:20:34 +0200

    use of laracasts/presenter

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -1,14 +1,14 @@
 <?php
 namespace TypiCMS\Modules\Menus\Presenters;
 
-use TypiCMS\Presenters\Presenter;
+use Laracasts\Presenter\Presenter;
 
 class ModulePresenter extends Presenter
 {
 
     /**
      * Get title
-     * 
+     *
      * @return string
      */
     public function title()
