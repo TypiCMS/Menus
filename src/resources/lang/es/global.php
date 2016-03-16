237d0f12088b89111958ed76bbd398de14938442
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 11:49:25 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'name'                            => 'Menús',
+    'menus'                           => 'menu|menus',
+    'New'                             => 'Nuevo menú',
+    'Edit'                            => 'Editar menú',
+    'Back'                            => 'Volver a los menús',
+    'No menu found with name “:name”' => 'No se ha encontrado un menú con el nombre “:name”.',
+    'Active tab'                      => 'Pestaña activa',
+    'New tab'                         => 'Nueva pestaña',
+    'New menulink'                    => 'Nuevo enlace de menú',
+    'Edit menulink'                   => 'Editar enlace del menú',
+    'Back to menu'                    => 'Volver al menú',
+];
