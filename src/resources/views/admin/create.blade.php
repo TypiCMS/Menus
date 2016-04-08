c850ca8577b6e3d6a61ce2387a776f9518284b93
Samuel De Backer <sdebacker@gmail.com>
2016-04-08 17:04:28 +0200

    views

---
+++ src/resources/views/admin/create.blade.php
@@ -0,0 +1,16 @@
+@extends('core::admin.master')
+
+@section('title', trans('menus::global.New'))
+
+@section('main')
+
+    @include('core::admin._button-back', ['module' => 'menus'])
+    <h1>
+        @lang('menus::global.New')
+    </h1>
+
+    {!! BootForm::open()->action(route('admin::index-menus'))->multipart()->role('form') !!}
+        @include('menus::admin._form')
+    {!! BootForm::close() !!}
+
+@endsection
