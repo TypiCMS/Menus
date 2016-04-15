3f488be1d6743b8d3a02ddee6499c8ed5142442d
Samuel De Backer <sdebacker@gmail.com>
2016-04-15 19:38:46 +0200

    validation

--- src/Http/Requests/MenulinkFormRequest.php
+++ src/Http/Requests/MenulinkFormRequest.php
@@ -8,17 +8,13 @@ class MenulinkFormRequest extends AbstractFormRequest
 {
     public function rules()
     {
-        $rules = [
+        return [
             'menu_id'    => 'required',
             'page_id'    => 'required_if:has_categories,1',
             'class'      => 'max:255',
             'icon_class' => 'max:255',
+            'title.*'    => 'max:255',
+            'url.*'      => 'url|max:255',
         ];
-        foreach (config('translatable.locales') as $locale) {
-            $rules[$locale.'.title'] = 'max:255';
-            $rules[$locale.'.url'] = 'url|max:255';
-        }
-
-        return $rules;
     }
 }
