c20cc0b687e627dbcfe05c3f21e50587bba88074
Samuel De Backer <sdebacker@gmail.com>
2016-05-10 09:37:17 +0200

    @return null

--- src/database/migrations/2013_09_03_084148_create_menulinks_tables.php
+++ src/database/migrations/2013_09_03_084148_create_menulinks_tables.php
@@ -8,7 +8,7 @@ class CreateMenulinksTables extends Migration
     /**
      * Run the migrations.
      *
-     * @return void
+     * @return null
      */
     public function up()
     {
@@ -35,7 +35,7 @@ class CreateMenulinksTables extends Migration
     /**
      * Reverse the migrations.
      *
-     * @return void
+     * @return null
      */
     public function down()
     {
