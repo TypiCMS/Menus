b1b960132de239e75b9654287cd5e2afdd5d2d69
Samuel De Backer <sdebacker@gmail.com>
2017-01-19 10:02:09 +0100

    array alignment removed

--- src/resources/lang/en/global.php
+++ src/resources/lang/en/global.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'name'                            => 'Menus',
-    'menus'                           => 'menu|menus',
-    'New'                             => 'New menu',
-    'Edit'                            => 'Edit menu',
-    'Back'                            => 'Back to menus',
+    'name' => 'Menus',
+    'menus' => 'menu|menus',
+    'New' => 'New menu',
+    'Edit' => 'Edit menu',
+    'Back' => 'Back to menus',
     'No menu found with name “:name”' => 'No menu found with name “:name”.',
-    'Active tab'                      => 'Active tab',
-    'New tab'                         => 'New tab',
-    'New menulink'                    => 'New menu link',
-    'Edit menulink'                   => 'Edit menu link',
-    'Back to menu'                    => 'Back to menu',
+    'Active tab' => 'Active tab',
+    'New tab' => 'New tab',
+    'New menulink' => 'New menu link',
+    'Edit menulink' => 'Edit menu link',
+    'Back to menu' => 'Back to menu',
 ];
