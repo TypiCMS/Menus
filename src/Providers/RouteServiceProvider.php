7229173ad93eae7e93c013d6b4c7739b05065917
Samuel De Backer <sdebacker@gmail.com>
2018-09-18 14:55:53 +0200

    auth:api middleware

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -45,14 +45,16 @@ class RouteServiceProvider extends ServiceProvider
              * API routes
              */
             $router->middleware('api')->prefix('api')->group(function (Router $router) {
-                $router->get('menus', 'ApiController@index')->name('api::index-menus');
-                $router->patch('menus/{menu}', 'ApiController@update')->name('api::update-menu');
-                $router->delete('menus/{menu}', 'ApiController@destroy')->name('api::destroy-menu');
+                $router->middleware('auth:api')->group(function (Router $router) {
+                    $router->get('menus', 'ApiController@index')->name('api::index-menus');
+                    $router->patch('menus/{menu}', 'ApiController@update')->name('api::update-menu');
+                    $router->delete('menus/{menu}', 'ApiController@destroy')->name('api::destroy-menu');
 
-                $router->get('menus/{menu}/menulinks', 'MenulinksApiController@index')->name('api::index-menulinks');
-                $router->patch('menus/{menu}/menulinks/{menulink}', 'MenulinksApiController@update')->name('api::update-menulink');
-                $router->post('menus/{menu}/menulinks/sort', 'MenulinksApiController@sort')->name('api::sort-menulinks');
-                $router->delete('menus/{menu}/menulinks/{menulink}', 'MenulinksApiController@destroy')->name('api::destroy-menulink');
+                    $router->get('menus/{menu}/menulinks', 'MenulinksApiController@index')->name('api::index-menulinks');
+                    $router->patch('menus/{menu}/menulinks/{menulink}', 'MenulinksApiController@update')->name('api::update-menulink');
+                    $router->post('menus/{menu}/menulinks/sort', 'MenulinksApiController@sort')->name('api::sort-menulinks');
+                    $router->delete('menus/{menu}/menulinks/{menulink}', 'MenulinksApiController@destroy')->name('api::destroy-menulink');
+                });
             });
         });
     }
