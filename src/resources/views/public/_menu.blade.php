dbf10cdabd200477179cb39ca3031a129ec7f1a3
Samuel De Backer <sdebacker@gmail.com>
2018-09-27 16:46:37 +0200

    count() > 0

--- src/resources/views/public/_menu.blade.php
+++ src/resources/views/public/_menu.blade.php
@@ -1,6 +1,6 @@
 @if ($menu = Menus::getMenu($name))
 
-    @if ($menu->menulinks->count())
+    @if ($menu->menulinks->count() > 0)
     <ul class="menu-{{ $name }}-list {{ $menu->class }}" role="menu">
         @foreach ($menu->menulinks as $menulink)
             @include('menus::public._item')
