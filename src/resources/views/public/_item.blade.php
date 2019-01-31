81dd2bc025af303f4f3e1c0d9b4b59ac627f5b9d
Samuel De Backer <sdebacker@gmail.com>
2019-01-31 20:11:41 +0100

    static dropdown

--- src/resources/views/public/_item.blade.php
+++ src/resources/views/public/_item.blade.php
@@ -1,5 +1,5 @@
 <li class="menu-{{ $name }}-item menu-{{ $name }}-item-{{ $menulink->id }} {{ $menulink->class }}" id="menuitem_{{ $menulink->id }}" role="menuitem">
-    <a class="menu-{{ $name }}-link {{ $menulink->items->count() > 0 ? 'dropdown-toggle' : '' }}" href="{{ url($menulink->href) }}" @if ($menulink->target === '_blank') target="_blank" rel="noopener noreferrer" @endif @if ($menulink->items->count() > 0) data-toggle="dropdown" @endif>
+    <a class="menu-{{ $name }}-link {{ $menulink->items->count() > 0 ? 'dropdown-toggle' : '' }}" href="{{ url($menulink->href) }}" @if ($menulink->target === '_blank') target="_blank" rel="noopener noreferrer" @endif @if ($menulink->items->count() > 0) data-toggle="dropdown" data-display="static" @endif>
         @if ($menulink->icon_class)
             <span class="{{ $menulink->icon_class }}"></span>
         @endif
