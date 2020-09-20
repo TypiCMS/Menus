52ed51efd6f608299d3124c7dd46f9aca0b74045
Samuel De Backer <sdebacker@gmail.com>
2020-09-20 20:54:31 +0200

    permissions

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -24,12 +24,12 @@ class RouteServiceProvider extends ServiceProvider
             $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
                 $router->get('menus', [AdminController::class, 'index'])->name('admin::index-menus')->middleware('can:read menus');
                 $router->get('menus/create', [AdminController::class, 'create'])->name('admin::create-menu')->middleware('can:create menus');
-                $router->get('menus/{menu}/edit', [AdminController::class, 'edit'])->name('admin::edit-menu')->middleware('can:update menus');
+                $router->get('menus/{menu}/edit', [AdminController::class, 'edit'])->name('admin::edit-menu')->middleware('can:read menus');
                 $router->post('menus', [AdminController::class, 'store'])->name('admin::store-menu')->middleware('can:create menus');
                 $router->put('menus/{menu}', [AdminController::class, 'update'])->name('admin::update-menu')->middleware('can:update menus');
 
                 $router->get('menus/{menu}/menulinks/create', [MenulinksAdminController::class, 'create'])->name('admin::create-menulink')->middleware('can:create menulinks');
-                $router->get('menus/{menu}/menulinks/{menulink}/edit', [MenulinksAdminController::class, 'edit'])->name('admin::edit-menulink')->middleware('can:update menulinks');
+                $router->get('menus/{menu}/menulinks/{menulink}/edit', [MenulinksAdminController::class, 'edit'])->name('admin::edit-menulink')->middleware('can:read menulinks');
                 $router->post('menus/{menu}/menulinks', [MenulinksAdminController::class, 'store'])->name('admin::store-menulink')->middleware('can:create menulinks');
                 $router->put('menus/{menu}/menulinks/{menulink}', [MenulinksAdminController::class, 'update'])->name('admin::update-menulink')->middleware('can:update menulinks');
             });
