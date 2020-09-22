5b0db643e5561a51109fc19f34531731104b4e85
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 16:11:52 +0200

    sidebar icon

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -19,7 +19,7 @@ class SidebarViewComposer
             $group->weight = 30;
             $group->addItem(__('Menus'), function (SidebarItem $item) {
                 $item->id = 'menus';
-                $item->icon = config('typicms.menus.sidebar.icon', 'icon fa fa-fw fa-bars');
+                $item->icon = config('typicms.menus.sidebar.icon');
                 $item->weight = config('typicms.menus.sidebar.weight');
                 $item->route('admin::index-menus');
                 $item->append('admin::create-menu');
