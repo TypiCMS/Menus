9f38c9a83224f7cd50035d14222ba56a0a50e153
Samuel De Backer <sdebacker@gmail.com>
2021-11-09 23:25:49 +0100

    menu classes changed

--- resources/views/public/_item.blade.php
+++ resources/views/public/_item.blade.php
@@ -1,12 +1,12 @@
-<li class="menu-{{ $name }}-item menu-{{ $name }}-item-{{ $menulink->id }} {{ $menulink->class }}" id="menuitem_{{ $menulink->id }}" role="menuitem">
-    <a class="menu-{{ $name }}-link {{ $menulink->items->count() > 0 ? 'dropdown-toggle' : '' }}{{ !empty($menulink->parent) ? 'dropdown-item' : '' }}" href="{{ $menulink->items->count() > 0 ? '#' : url($menulink->href) }}" @if ($menulink->target === '_blank') target="_blank" rel="noopener noreferrer" @endif @if ($menulink->items->count() > 0) role="button" id="menuitem_{{ $menulink->id }}_id" data-bs-toggle="dropdown" aria-expanded="false" @endif>
+<li class="{{ $name }}-nav-item {{ $name }}-nav-item-{{ $menulink->id }} {{ $menulink->class }}" id="menuitem_{{ $menulink->id }}" role="menuitem">
+    <a class="{{ $name }}-nav-link {{ $menulink->items->count() > 0 ? 'dropdown-toggle' : '' }}{{ !empty($menulink->parent) ? 'dropdown-item' : '' }}" href="{{ $menulink->items->count() > 0 ? '#' : url($menulink->href) }}" @if ($menulink->target === '_blank') target="_blank" rel="noopener noreferrer" @endif @if ($menulink->items->count() > 0) role="button" id="menuitem_{{ $menulink->id }}_id" data-bs-toggle="dropdown" aria-expanded="false" @endif>
         @if ($menulink->image !== null)
-            <img class="menu-{{ $name }}-image" aria-hidden="true" src="{{ $menulink->present()->image }}" width="32" height="32" alt="">
+            <img class="{{ $name }}-nav-image" aria-hidden="true" src="{{ $menulink->present()->image }}" width="32" height="32" alt="">
         @endif
-        <span class="menu-{{ $name }}-label">{{ $menulink->title }}</span>
+        <span class="{{ $name }}-nav-label">{{ $menulink->title }}</span>
     </a>
     @if ($menulink->items->count() > 0)
-        <ul class="menu-{{ $name }}-dropdown dropdown-menu" aria-labelledby="menuitem_{{ $menulink->id }}_id">
+        <ul class="{{ $name }}-nav-dropdown dropdown-menu" aria-labelledby="menuitem_{{ $menulink->id }}_id">
             @foreach ($menulink->items as $menulink)
                 @include('menus::public._item', ['menulink' => $menulink])
             @endforeach
--- resources/views/public/_menu.blade.php
+++ resources/views/public/_menu.blade.php
@@ -1,7 +1,7 @@
 @if ($menu = Menus::getMenu($name))
 
     @if ($menu->menulinks->count() > 0)
-    <ul class="menu-{{ $name }}-list {{ $menu->class }}" role="menu">
+    <ul class="{{ $name }}-nav-list {{ $menu->class }}" role="menu">
         @foreach ($menu->menulinks as $menulink)
             @include('menus::public._item')
         @endforeach
